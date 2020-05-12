294cd3b7d4df4b3b748e77c2a5611953089786ff
loovjo <jonathan.loov@gmail.com>
2020-05-12 22:59:35 +0200

    Track dark/light mode in localStorage

--- scripts.js
+++ scripts.js
@@ -3,9 +3,15 @@ function toggle_dark_mode() {
 	if (colourMode == 0) {
 		// Dark?
 		document.getElementsByTagName('body')[0].classList.add("dark-mode");
+		localStorage.setItem("mode", "dark");
 	} else {
 		// Standard
 		document.getElementsByTagName('body')[0].classList.remove("dark-mode");
+		localStorage.setItem("mode", "light");
 	}
 	colourMode = (colourMode + 1) % 2;
+}
+
+if (localStorage.getItem("mode") === "dark") {
+    window.onload = toggle_dark_mode;
 }
