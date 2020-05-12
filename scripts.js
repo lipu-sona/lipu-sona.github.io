993b1921271bf96b01c11423fb89878f24e43018
loovjo <jonathan.loov@gmail.com>
2020-05-12 23:48:51 +0200

    Transitions!

--- scripts.js
+++ scripts.js
@@ -1,17 +1,26 @@
+var body;
+
 colourMode = 0;
 function toggle_dark_mode() {
 	if (colourMode == 0) {
 		// Dark?
-		document.getElementsByTagName('body')[0].classList.add("dark-mode");
+		body.classList.add("dark-mode");
 		localStorage.setItem("mode", "dark");
 	} else {
 		// Standard
-		document.getElementsByTagName('body')[0].classList.remove("dark-mode");
+		body.classList.remove("dark-mode");
 		localStorage.setItem("mode", "light");
 	}
 	colourMode = (colourMode + 1) % 2;
 }
 
-if (localStorage.getItem("mode") === "dark") {
-    window.onload = toggle_dark_mode;
-}
+document.addEventListener("DOMContentLoaded", function() {
+	window.body = document.getElementsByTagName('body')[0];
+	if (localStorage.getItem("mode") === "dark") {
+
+		body.classList.add("notransition");
+		toggle_dark_mode();
+		body.offsetHeight;
+		body.classList.remove("notransition");
+    }
+});
