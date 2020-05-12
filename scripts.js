876e3150ce981c593410926e9676c31b0ab0506d
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2020-05-13 00:43:43 +0500

    Tweaks and stubs

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ colourMode = 0;
 function toggle_dark_mode() {
 	if (colourMode == 0) {
 		// Dark?
-		document.getElementsByTagName('html')[0].style.cssText = "--bg-color: #000000; --hl-color: #eeeeee; --ac-color: #444444; --fg-color: #ffffff;";
-	} else {
+		document.getElementsByTagName('html')[0].style.cssText = "--main-bg-color: #000000; --highlight-bg-color: #eeeeee; --opposite-bg-color: #ffffff; --main-txt-color: #ffffff; --opposite-txt-color: #444444;";
+	} else { 
 		// Standard
-		document.getElementsByTagName('html')[0].style.cssText = "--bg-color: #092e37; --hl-color: #e8f1f3; --ac-color: #659fad; --fg-color: #ffffff;";
+		document.getElementsByTagName('html')[0].style.cssText = " --main-bg-color: #092e37; --highlight-bg-color: #e8f1f3; --opposite-bg-color: #ffffff; --main-txt-color: #ffffff; --opposite-txt-color: #659fad;";
 	}
 	colourMode = (colourMode + 1) % 2;
 }
