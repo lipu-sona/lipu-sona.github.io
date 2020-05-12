c4ef2f5d1b1facdaa024562571921ed1480951e2
loovjo <jonathan.loov@gmail.com>
2020-05-12 22:39:32 +0200

    Make dark mode be a class of body, to allow better control and lighter javascript

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ colourMode = 0;
 function toggle_dark_mode() {
 	if (colourMode == 0) {
 		// Dark?
-		document.getElementsByTagName('html')[0].style.cssText = "--main-bg-color: #000000; --highlight-bg-color: #eeeeee; --opposite-bg-color: #ffffff; --main-txt-color: #ffffff; --opposite-txt-color: #444444;";
-	} else { 
+		document.getElementsByTagName('body')[0].classList.add("dark-mode");
+	} else {
 		// Standard
-		document.getElementsByTagName('html')[0].style.cssText = " --main-bg-color: #092e37; --highlight-bg-color: #e8f1f3; --opposite-bg-color: #ffffff; --main-txt-color: #ffffff; --opposite-txt-color: #659fad;";
+		document.getElementsByTagName('body')[0].classList.remove("dark-mode");
 	}
 	colourMode = (colourMode + 1) % 2;
 }
